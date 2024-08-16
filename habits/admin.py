c38d94ea894c974d484d6b5c3d4f27ffa2e1095f
Nanu25 <alexandrugrancea25@gmail.com>
2024-08-16 12:13:00 +0300

    status bar added

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Habit
+
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ("habit", "description", "status", "times_completed")
+
+admin.site.register(User)
+admin.site.register(Habit, HabitAdmin)
